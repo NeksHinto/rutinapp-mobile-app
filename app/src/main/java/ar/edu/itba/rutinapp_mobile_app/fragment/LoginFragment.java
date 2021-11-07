a2b1a14ce93886714f4c80a2be54f0b713628609
Azul <azkim@itba.edu.ar>
2021-11-07 15:50:30 -0300

    Login added

---
+++ app/src/main/java/ar/edu/itba/rutinapp_mobile_app/fragment/LoginFragment.java
@@ -0,0 +1,39 @@
+package ar.edu.itba.rutinapp_mobile_app.fragment;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ar.edu.itba.rutinapp_mobile_app.R;
+
+public class LoginFragment extends Fragment {
+
+    private UserViewModel mViewModel;
+
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.login_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/ar/edu/itba/rutinapp_mobile_app/fragment/UserViewModel.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.rutinapp_mobile_app.fragment;
+
+import androidx.lifecycle.ViewModel;
+
+public class UserViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
