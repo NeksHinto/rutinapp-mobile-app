afb18391faf665a7b75163b6385955ed7df6917c
dlimachi <desiree.limachi@gmail.com>
2021-11-17 19:07:38 -0300

    info fragment como dialogo

---
+++ app/src/main/java/ar/edu/itba/rutinapp_mobile_app/fragment/InformationFragment.java
@@ -0,0 +1,65 @@
+package ar.edu.itba.rutinapp_mobile_app.fragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import ar.edu.itba.rutinapp_mobile_app.R;
+
+public class InformationFragment extends DialogFragment {
+
+    public interface InformationListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    InformationListener listener;
+
+    public InformationFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.fragment_information, null))
+                // Add action buttons
+                .setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        cancelar();
+                    }
+                });
+        return builder.show();
+    }
+
+    private void cancelar() {
+        
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_information, container, false);
+    }
+}
