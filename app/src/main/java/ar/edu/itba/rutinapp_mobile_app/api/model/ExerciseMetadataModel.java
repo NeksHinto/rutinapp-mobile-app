0f5631611cb86f31ffc2df2b27c189eacbbc48ea
Azul <azkim@itba.edu.ar>
2021-11-21 17:30:43 -0300

    Create exercise metadata

---
+++ app/src/main/java/ar/edu/itba/rutinapp_mobile_app/api/model/ExerciseMetadataModel.java
@@ -0,0 +1,36 @@
+package ar.edu.itba.rutinapp_mobile_app.api.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ExerciseMetadataModel {
+    @SerializedName("series")
+    @Expose
+    private Integer series;
+
+    @SerializedName("duration")
+    @Expose
+    private Integer duration;
+
+    @SerializedName("difficulty")
+    @Expose
+    private String difficulty;
+
+    @SerializedName("image")
+    @Expose
+    private String image;
+
+    @SerializedName("category")
+    @Expose
+    private String category;
+
+    public ExerciseMetadataModel(Integer series, Integer duration, String difficulty, String image, String category) {
+        this.series = series;
+        this.duration = duration;
+        this.image = image;
+        this.category = category;
+    }
+
+    public Integer getSeries() { return this.series; }
+    
+}
