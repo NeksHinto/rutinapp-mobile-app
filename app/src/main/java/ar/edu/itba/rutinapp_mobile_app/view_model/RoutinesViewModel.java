74b434519c321efeaaf8b489c891cfd6df96a8c2
dlimachi <desiree.limachi@gmail.com>
2021-11-21 20:22:57 -0300

    adaptacion de tamaños

---
+++ app/src/main/java/ar/edu/itba/rutinapp_mobile_app/view_model/RoutinesViewModel.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.rutinapp_mobile_app.view_model;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class RoutinesViewModel extends AndroidViewModel {
+
+    public RoutinesViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
