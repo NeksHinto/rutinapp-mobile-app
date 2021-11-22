5cffe55ecca884405d0053c832893fb799f37797
dlimachi <desiree.limachi@gmail.com>
2021-11-22 10:37:21 -0300

    cambios iconos

--- app/src/main/java/ar/edu/itba/rutinapp_mobile_app/fragment/ViewRountineFragment.java
+++ app/src/main/java/ar/edu/itba/rutinapp_mobile_app/fragment/ViewRountineFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -161,8 +162,9 @@ public class ViewRountineFragment extends Fragment {
         });
 
         startBtn.setOnClickListener(v->{
-            NavDirections action = ViewRountineFragmentDirections.actionViewRountineFragmentToActiveRoutineFragment2();
-            Navigation.findNavController(v).navigate(action);
+            NavHostFragment.findNavController(this).navigate(R.id.action_viewRountineFragment_to_activeRoutineFragment);
+            //NavDirections action = ViewRountineFragmentDirections.actionViewRountineFragmentToActiveRoutineFragment2;
+            //Navigation.findNavController(v).navigate(action);
         });
 
     });
