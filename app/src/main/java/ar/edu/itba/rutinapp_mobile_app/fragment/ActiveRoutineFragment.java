c6e1cb570553e3f6aac4192e56840793beb44d9e
dlimachi <desiree.limachi@gmail.com>
2021-11-21 23:53:54 -0300

    cambios en active routine

--- app/src/main/java/ar/edu/itba/rutinapp_mobile_app/fragment/ActiveRoutineFragment.java
+++ app/src/main/java/ar/edu/itba/rutinapp_mobile_app/fragment/ActiveRoutineFragment.java
@@ -7,15 +7,41 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import ar.edu.itba.rutinapp_mobile_app.R;
+import ar.edu.itba.rutinapp_mobile_app.api.model.ExerciseModel;
+import ar.edu.itba.rutinapp_mobile_app.databinding.FragmentActiveRoutineBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ActiveRoutineFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ActiveRoutineFragment extends Fragment {
+
+    private FragmentActiveRoutineBinding binding;
+    private ExerciseModel viewModel;
+
+    private View view;
+    private TextView title;
+    private TextView timeExercise;
+    private ProgressBar progressBar;
+
+    private int currentCycle;
+    private String cycleTitle;
+    private ArrayList<ExerciseModel> currCycleIndex;
+    private int currentExerciseIndex;
+    private boolean finished;
+    private ExerciseModel currentExercise;
+    private boolean executed;
+    private int status;
+
+    private static final int WARMUP_CYCLE = 0;
+    private static final int MAIN_CYCLE = 1;
+    private static final int COOLDOWN_CYCLE = 2;
+    private static final int PLAYING = 0;
+    private static final int PAUSED = 1;
+    private static final int NOTRUNNING = 2;
+    private static final int REPS_TIME= 10;
 
     public ActiveRoutineFragment() {
         // Required empty public constructor
@@ -36,6 +62,43 @@ public class ActiveRoutineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_active_routine, container, false);
+        binding = FragmentActiveRoutineBinding.inflate(getLayoutInflater());
+        title = binding.NameExercise1;
+        //timeExercise = binding.timeExercise;
+        view = binding.getRoot();
+
+        binding.play.setOnClickListener(v -> playExecution());
+        binding.pause.setOnClickListener(v -> pauseExecution());
+        binding.nextExercise.setOnClickListener(v -> nextExecution());
+        binding.previousExercise.setOnClickListener(v -> previousExecution());
+        binding.previousExercise.setVisibility(View.GONE);
+        binding.nextExercise.setVisibility(View.GONE);
+        //return inflater.inflate(R.layout.fragment_active_routine, container, false);
+
+        return view;
+    }
+
+    private void playExecution() {
+        binding.play.setVisibility(View.INVISIBLE);
+        binding.pause.setVisibility(View.VISIBLE);
+        finished = false;
+        status = PLAYING;
+
+    }
+
+    private void pauseExecution() {
+        binding.play.setVisibility(View.VISIBLE);
+        binding.pause.setVisibility(View.INVISIBLE);
+        status = PAUSED;
+    }
+
+    private void nextExecution() {
+        currentExerciseIndex++;
+
+    }
+
+    private void previousExecution() {
+        currentExerciseIndex--;
+
     }
 }
